7d7ea9bea35ba60189400b0a3a3bafc78539cf97
WuJiayiSH <wujiayish@msn.com>
2022-06-09 11:57:17 +0800

    Initialized from 3addfa5f36870522dce2380bbe3fab91376d0fd4

---
+++ display/FUILabel.h
@@ -0,0 +1,45 @@
+#ifndef __FUILABEL_H__
+#define __FUILABEL_H__
+
+#include "cocos2d.h"
+#include "FairyGUIMacros.h"
+#include "TextFormat.h"
+
+NS_FGUI_BEGIN
+
+class FUILabel : public cocos2d::Label
+{
+public:
+    FUILabel();
+    virtual ~FUILabel();
+
+    CREATE_FUNC(FUILabel);
+
+    const std::string& getText() const { return getString(); }
+    void setText(const std::string& value);
+
+    TextFormat* getTextFormat() const { return _textFormat; }
+    void applyTextFormat();
+
+    // NOTE: cocos2d-x doesn't support setting seperate color to underline,
+    // and I really think what richtext need is to set text color as a whole for links
+    // void setUnderlineColor(const cocos2d::Color3B& value);
+
+    virtual bool setBMFontFilePath(const std::string& bmfontFilePath, const cocos2d::Vec2& imageOffset = cocos2d::Vec2::ZERO, float fontSize = 0) override;
+
+    void setGrayed(bool value);
+protected:
+    /* 2d/CCLabel.h add `virtual` defore `void updateBMFontScale()` */
+    virtual void updateBMFontScale() override;
+
+private:
+    TextFormat* _textFormat;
+    std::string _fontName;
+    int _fontSize;
+    bool _bmFontCanTint;
+    bool _grayed;
+};
+
+NS_FGUI_END
+
+#endif
